c6cc579499d555c737b0eb76e2b50638d228a89a
Basil Njoga <Basilnjoga@gmail.com>
2023-09-09 21:46:52 +0300

    fixed date time format

--- create-and-host-endpoint/main.py
+++ create-and-host-endpoint/main.py
@@ -6,7 +6,6 @@ from datetime import datetime, timezone
 app = FastAPI()
 
 currentDateTime = datetime.now(timezone.utc)
-utcTime = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 #api methods
@@ -14,7 +13,7 @@ utcTime = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 async def root(slack_name: str, track: str):
     return {"slack_name":  slack_name,
             "current_day": currentDateTime.strftime('%A'),
-            "utc_time": utcTime,
+            "utc_time": currentDateTime.strftime("%Y-%m-%dT%H:%M:%SZ"),
             "track": track,
             "github_file_url": "https://github.com/BasilNjoga/zuri-backend/blob/main/create-and-host-endpoint/main.py",
             "github_repo_url": "https://github.com/BasilNjoga/zuri-backend/tree/main/create-and-host-endpoint",

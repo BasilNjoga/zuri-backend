f3e589c14423adcfb2ed39c9dfdb4d852b3b27d0
Basil Njoga <Basilnjoga@gmail.com>
2023-09-12 17:06:03 +0300

    updated api endpoints

--- restapi-crud-operations/person/urls.py
+++ restapi-crud-operations/person/urls.py
@@ -14,8 +14,6 @@ urlpatterns = [
     path("create/", PersonDetailCreate.as_view(), name="create-person"),
     path("", PersonDetailList.as_view()),
     path("<int:pk>/", PersonDetails.as_view(), name="retrieve-person-details"),
-    path(
-        "update/<int:pk>/", PersonDetailUpdate.as_view(), name="update-person-details"
-    ),
-    path("delete/<int:pk>/", PersonDetailDelete.as_view(), name="delete-person"),
+    path("<int:pk>/", PersonDetailUpdate.as_view(), name="update-person-details"),
+    path("<int:pk>/", PersonDetailDelete.as_view(), name="delete-person"),
 ]
